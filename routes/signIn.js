e0e6cd4155dada5103c365377a337e9b1cf603a6
Arman Urazov <aurazov@myseneca.ca>
2022-03-05 03:11:02 -0500

    bcrypt replaced by bcryptjs

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const route = express.Router();
 const userModel = require('../models/users');
 // const clientSessions = require("client-sessions");
